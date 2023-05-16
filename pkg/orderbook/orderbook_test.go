487d3362c20b4fa08f23ac207726a32b83d3bc55
shakezula <lott.dylan@gmail.com>
2023-05-15 22:16:21 -0600

    benchmark tests for AttemptFill

--- pkg/orderbook/orderbook_test.go
+++ pkg/orderbook/orderbook_test.go
@@ -336,6 +336,33 @@ func BenchmarkMatchOrder(b *testing.B) {
 	fmt.Printf("got #: %v\n", len(got))
 }
 
+func BenchmarkAttemptFill(b *testing.B) {
+	ctx := context.Background()
+	wg := &sync.WaitGroup{}
+
+	accts := &accounts.InMemoryManager{}
+	writes := make(chan OpWrite, bufferSize)
+	errs := make(chan error, bufferSize)
+	fills := make(chan FillResult, bufferSize)
+
+	go Start(ctx, accts, writes, fills, errs)
+
+	for i := 0; i < b.N; i++ {
+		w := OpWrite{
+			Order:  newOrder(fmt.Sprintf("%d", i), ""),
+			Result: make(chan WriteResult),
+		}
+		go func() {
+			<-w.Result
+			wg.Done()
+		}()
+		wg.Add(1)
+		writes <- w
+	}
+
+	wg.Wait()
+}
+
 func newTestOrders(count int) (buyOrders []Order, sellOrders []Order) {
 	log.Printf("count %d", count)
 	rand.Seed(time.Now().UnixNano())
@@ -365,4 +392,30 @@ func newTestOrders(count int) (buyOrders []Order, sellOrders []Order) {
 	}
 
 	return
+}
+
+func newOrder(id, account string) Order {
+	rand.Seed(time.Now().UnixNano())
+
+	var minPrice, maxPrice = 100, 10_000
+	var minOpen, maxOpen = 10, 1_000_000
+
+	o := Order{
+		ID:        id,
+		AccountID: account, // TODO: add a random account owner
+		Kind:      "market",
+		Price:     uint64(rand.Intn(maxPrice-minPrice) + minPrice),
+		Open:      uint64(rand.Intn(maxOpen-minOpen) + minOpen),
+		Filled:    0,
+		History:   []Match{}, // history should be nil
+	}
+
+	// half buy, half sell orders
+	if o.Price%2 == 0 {
+		o.Side = "buy"
+	} else {
+		o.Side = "sell"
+	}
+
+	return o
 }
